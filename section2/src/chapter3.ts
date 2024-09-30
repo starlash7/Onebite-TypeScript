c87e8a2754f54a8c204393bba27fe449014d8ea5
starlash7 <yang92sw@gmail.com>
2024-09-30 16:37:07 +0900

    객체

---
+++ section2/src/chapter3.ts
@@ -0,0 +1,16 @@
+// object
+let user: {
+  id?: number;
+  name: string;
+} = {
+  id: 1,
+  name: "이정환",
+};
+
+let config: {
+  readonly apiKey: string;
+} = {
+  apiKey: `MY API KEY`,
+};
+
+config.apiKey = "hacked";
