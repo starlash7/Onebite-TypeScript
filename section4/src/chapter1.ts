f0498de775fae28017111616cbe899ee98a1c067
starlash7 <yang92sw@gmail.com>
2024-10-01 11:09:51 +0900

    함수 호출시그니처

---
+++ section4/src/chapter1.ts
@@ -0,0 +1,27 @@
+/**
+ * 함수 타입 표현식
+ */
+
+type Operation = (a: number, b: number) => number;
+
+const add: (a: number, b: number) => number = (a, b) => a + b;
+const sub: Operation = (a, b) => a - b;
+const mul: Operation = (a, b) => a * b;
+const div: Operation = (a, b) => a / b;
+
+/**
+ * 호출 시그니처
+ * 콜 시그니처
+ */
+
+type Operation2 = {
+  (a: number, b: number): number;
+  name: string;
+};
+
+function func(a: number): void {}
+
+const add2: Operation2 = (a, b) => a + b;
+const sub2: Operation2 = (a, b) => a - b;
+const mul2: Operation2 = (a, b) => a * b;
+const div2: Operation2 = (a, b) => a / b;
